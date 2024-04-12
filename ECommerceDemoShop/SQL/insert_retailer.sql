b44faeed4b0aab7362f52293addcd72c2331e810
Andreas Englhart <andreas.englhart@ottogroup.com>
2024-04-12 16:04:08 +0200

    add additional tables and data for assessment

---
+++ ECommerceDemoShop/SQL/insert_retailer.sql
@@ -0,0 +1,10 @@
+INSERT INTO core.retailer (retailer_id,retailer_name,retailer_type,retailer_level,retailer_address,zip,city) VALUES
+	 (1000,'WERNER','Retail','NULL','Freiberger Strasse 35','01067','Dresden'),
+	 (3913,'Chic Boutique','Retail','NULL','Hartplatz 39','12489','Berlin'),
+	 (3928,'Glamour Gallery','Retail','NULL','Kirchenweg 12','04347','Leipzig'),
+	 (3510,'Dapper Deals','Retail','NULL','Freiheitsplatz 2','64293','Darmstadt'),
+	 (4008,'Chic Click','Retail','NULL','Innweg 58','94036','Passau'),
+	 (4010,'Trendsetter Paradise','Retail','NULL','Alsterkai 41','22395','Hamburg'),
+	 (4016,'Junction','Retail','NULL','Leerweg 404','33613','Bielefeld'),
+	 (4021,'RDS Market','Wholesale','NULL','Grafstrasse 38','70569','Stuttgart'),
+	 (4033,'Dresden Import','Wholesale','NULL','Berliner Weg 9','22395','Hamburg');
